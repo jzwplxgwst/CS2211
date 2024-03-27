3df607156dbde02448a40afb8b638885a9153136
James <146906294+jzwplxgwst@users.noreply.github.com>
2024-03-27 18:27:05 -0400

    Add files via upload

---
+++ asn1/asn1.c
@@ -0,0 +1,13 @@
+// CS2211 2023
+// Assignment number 1
+// James Wong
+// 251297165
+// jwong988
+// Sept 21 2023
+
+#include <stdio.h>
+int main ()
+{
+	printf ("Hello, CS2211 2023!\n");
+	return 0;
+}
